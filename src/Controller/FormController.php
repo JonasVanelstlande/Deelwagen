6d7d6d606fde695cc9846ae9a72adabf6069acb6
Jonas <vanelstlande.jonas@gmail.com>
2023-09-26 23:45:50 +0200

    Form data submit

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Kilometers;
 use App\Form\KilometerFormType;
+use App\Repository\KilometersRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,11 +13,36 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class FormController extends AbstractController
 {
-    #[Route('/form', name: 'form')]
-    public function create(): Response
+    private $kilometersRepository;
+    private $em;
+
+    public function __construct(KilometersRepository $kilometersRepository, EntityManagerInterface $em)
+    {
+        $this->kilometersRepository = $kilometersRepository;
+        $this->em = $em;
+    }
+
+    #[Route('/deelwagen', name: 'deelwagen')]
+    public function index(): Response
+    {
+        return $this->render('base.html.twig', [
+        ]);
+    }
+
+    #[Route('/deelwagen/form', name: 'form')]
+    public function create(Request $request): Response
     {
         $trip = new Kilometers();
         $form = $this->createForm(KilometerFormType::class, $trip);
+
+        dd($this->em);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $newTrip = $form->getData();
+            $newTrip->setTotalKm($newTrip->getEndKm() - $newTrip->getStartKm());
+
+        }
 
         return $this->render('form/index.html.twig', [
             'firstname' => 'Jonas',
--- src/Form/KilometerFormType.php
+++ src/Form/KilometerFormType.php
@@ -5,7 +5,7 @@ namespace App\Form;
 use App\Entity\Kilometers;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\FormEvent;
@@ -16,11 +16,21 @@ class KilometerFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('startKm', NumberType::class)
-            ->add('endKm', NumberType::class)
-            ->add('totalKm', NumberType::class, [
+            ->add('startKm', IntegerType::class, [
+                'attr' => [
+                    'placeholder' => "bv. 13400"                    
+                ]
+            ])
+            ->add('endKm', IntegerType::class, [
+                'attr' => [
+                    'placeholder' => "bv. 13500"                    
+                ]
+            ])
+            ->add('totalKm', IntegerType::class, [
                 'attr' => [
                     'readonly' => true,
+                    'value' => 0
+
                 ]
             ])
             ->add('date', DateType::class)
